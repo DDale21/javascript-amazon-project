95f91c8e823ba4fbe7bb444e408465fcd693c033
DDale21 <christiandalepancho@gmail.com>
2024-11-06 16:17:43 +1300

    Checkout: Add functionality of removing a product from cart after clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -24,3 +24,15 @@ export const addToCart = (productId) => {
     });
   }
 };
+
+export const removeFromCart = (productId) => {
+  const newCart = [];
+
+  cart.forEach((item) => {
+    if (item.productId != productId) {
+      newCart.push(item);
+    }
+  });
+
+  cart = newCart;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import * as productModule from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -6,7 +6,6 @@ let checkoutItemsHtml = '';
 
 cart.forEach((item) => {
   const product = productModule.getProductById(item.productId);
-
   checkoutItemsHtml += `
     <div class="cart-item-container">
       <div class="delivery-date">
@@ -31,7 +30,8 @@ cart.forEach((item) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-quantity-link" 
+            data-product-id="${product.id}">
               Delete
             </span>
           </div>
@@ -87,3 +87,10 @@ cart.forEach((item) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = checkoutItemsHtml;
+
+document.querySelectorAll('.js-delete-quantity-link').forEach((deleteLink) => {
+  deleteLink.addEventListener('click', () => {
+    const productId = deleteLink.dataset.productId;
+    removeFromCart(productId);
+  });
+});
