f3c33988031eade59186211372d2c80145e71776
DDale21 <christiandalepancho@gmail.com>
2024-12-01 19:33:48 +1300

    Testing: Add automated tests for addToCart. Make use of mock tests.

--- data/cart.js
+++ data/cart.js
@@ -2,19 +2,25 @@ import { getProductById } from "./products.js";
 import formatCurrency from "../scripts/utils/money.js";
 import { getDeliveryOptionById } from "./deliveryOptions.js";
 
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+export const loadFromStorage = () => {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
+
+loadFromStorage();
 
 export const saveToStorage = () => {
   localStorage.setItem('cart', JSON.stringify(cart));
