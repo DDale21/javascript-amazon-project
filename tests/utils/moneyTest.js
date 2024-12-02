2639408d87bcd00bbfe41b6960f8f238597c531d
DDale21 <christiandalepancho@gmail.com>
2024-12-03 00:16:21 +1300

    Testing: Add more unit test for formatCurrency. -Rounds down to nearest cents -Can work with negative values

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -12,4 +12,20 @@ describe('test suite: formatCurrency', () => {
   it('rounds up to nearest cents', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
   });
+
+  it('rounds down to the nearest cents', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+
+  it('works with negative values', () => {
+    expect(formatCurrency(-1050)).toEqual('-10.50');
+  });
+
+  it('rounds up to nearest cents in negative value', () => {
+    expect(formatCurrency(-1050.7)).toEqual('-10.51');
+  });
+
+  it('rounds down to nearest cents in negative value', () => {
+    expect(formatCurrency(-1050.3)).toEqual('-10.50');
+  });
 });
