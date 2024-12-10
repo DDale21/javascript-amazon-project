be1a98a2c68cfdbd7bcf2d9dff1057b472aeeefe
DDale21 <christiandalepancho@gmail.com>
2024-12-11 01:22:50 +1300

    Orders: Create orders.js to save orders. Add eventlistener to place order button to create an order using supersimplebackend.dev/orders backend.

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,5 +1,6 @@
 import { cart } from "../../data/cart-class.js";
 import formatCurrency from "../utils/money.js";
+import { addOrder, getOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary () {
   const totalBeforeTax = cart.calculateProductTotalPrice() + cart.calculateShippingTotal();
@@ -42,9 +43,30 @@ export function renderPaymentSummary () {
       <div class="payment-summary-money js-total-price">$${cart.calculateOrderTotalPrice()}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+  document.querySelector('.js-place-order').addEventListener(('click'), async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart.cartItems
+        })
+      });
+  
+      const order = await response.json();
+      addOrder(order);
+      
+    } catch(error) {
+      console.log(error);
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
